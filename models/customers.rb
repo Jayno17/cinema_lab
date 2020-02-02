b79c82faae16b56402bb35342086fdbd74c640f4
Jayno17 <julia.nowak@gmail.com>
2020-02-02 12:29:19 +0000

    added update function

--- models/customers.rb
+++ models/customers.rb
@@ -24,6 +24,13 @@ attr_accessor :name, :funds
     SqlRunner.run(sql, values)
   end
 
+  def update()
+    sql = "UPDATE customers
+    SET (name, funds) = ($1, $2)
+    WHERE id = $3"
+    values =[@name, @funds, @id]
+    SqlRunner.run(sql, values)
+  end
 
   def self.delete_all()
     sql = "DELETE FROM customers"
--- models/films.rb
+++ models/films.rb
@@ -25,6 +25,13 @@ attr_accessor :title, :price
     SqlRunner.run(sql, values)
   end
 
+  def update()
+    sql = "UPDATE films
+    SET (title, price) = ($1, $2)
+    WHERE id = $3"
+    values =[@title, @price, @id]
+    SqlRunner.run(sql, values)
+  end
 
   def self.delete_all()
     sql = "DELETE FROM films"
